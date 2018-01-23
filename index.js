5061445029afc77eaf66c2b719d7b089ed109baa
Rachel Ingle <rachelpost79@yahoo.com>
2018-01-23 21:44:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var name = "Joe";
 var height = 74;
 var message = `${name} is ${height} inches tall`;
-
+let secondMessage = `Hi, my name is ${name}, and I am ${height} tall`;
 module.exports = {name, height, message};        
 
 console.log(message);
+console.log(secondMessage);
